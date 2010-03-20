4151b14b4fb4e3cd5b5ffcebdc5573f6daed2062
Iztok Jeras <izi@marvin.(none)>
2010-03-20 14:27:26 +0100

    some recoding, due to simmulators having problems understanding the code

--- hdl/debouncer/debouncer_tb.v
+++ hdl/debouncer/debouncer_tb.v
@@ -2,16 +2,20 @@
 
 module debouncer_tb;
 
+// number of debounced signals
+localparam DW = 2;
+genvar d;
+
 // counter length (max debounce time / clock period)
 localparam CN = 10_000_000 / 20; 
 
 // list of local signals
 reg           clk;  // clock
-reg           d_i;  // debouncer inptu
-wire          d_o;  // debouncer output
+reg  [DW-1:0] d_i;  // debouncer inptu
+wire [DW-1:0] d_o;  // debouncer output
 
-integer i;          // loop variable
-integer n;          // number of bounces
+integer i [DW-1:0]; // loop variable
+integer n [DW-1:0]; // number of bounces
 integer t;          // time
 integer seed;       // random seed
 
@@ -28,53 +32,57 @@ always #10 clk = ~clk;
 // initialize random seed
 initial seed = 0;
 
-// test signal generation
-// keypress timing is asynchronous
-initial begin
-  // stable OFF before start for 50ms
-  d_i = 1'b0;
-  # 50_000_000;
-  // switch ON random pulses (max 10ms)
-  n = 80;
-  for (i=0; i<n; i=i+1) begin
-    // short pulses
-    d_i = 1'b1;
-    t = $dist_uniform(seed, 30, 5000);
-    # t;
-    // folowed by longer pauses
-    d_i = 1'b0;
-    t = $dist_uniform(seed, 10_000, 200_000);
-    # t;
-  end
-  // stable ON state for 50ms
-  d_i = 1'b1;
-  # 50_000_000;
-  // switch OFF random pulses (max 10ms)
-  n = 80;
-  for (i=0; i<n; i=i+1) begin
-    // short pulses
-    d_i = 1'b0;
-    t = $dist_uniform(seed, 30, 5000);
-    # t;
-    // folowed by longer pauses
-    d_i = 1'b1;
-    t = $dist_uniform(seed, 10_000, 200_000);
-    # t;
-  end
-  // stable OFF state at the end for 50ms
-  d_i = 1'b0;
-  # 50_000_000;
-  // end simulation
-  $finish();
-end
+generate for (d=0; d<DW; d=d+1) begin
 
-// instantiate RTL
-debouncer #(
-  .CN   (CN)
-) debouncer_i (
-  .clk  (clk),
-  .d_i  (d_i),
-  .d_o  (d_o)
-);
+  // test signal generation
+  // keypress timing is asynchronous
+  initial begin
+    // stable OFF before start for 50ms
+    d_i[d] = 1'b0;
+    # 50_000_000;
+    // switch ON random pulses (max 10ms)
+    n[d] = 80;
+    for (i[d]=0; i[d]<n[d]; i[d]=i[d]+1) begin
+      // short pulses
+      d_i[d] = 1'b1;
+      t = $dist_uniform(seed, 30, 5000);
+      # t;
+      // folowed by longer pauses
+      d_i[d] = 1'b0;
+      t = $dist_uniform(seed, 10_000, 200_000);
+      # t;
+    end
+    // stable ON state for 50ms
+    d_i[d] = 1'b1;
+    # 50_000_000;
+    // switch OFF random pulses (max 10ms)
+    n[d] = 80;
+    for (i[d]=0; i[d]<n[d]; i[d]=i[d]+1) begin
+      // short pulses
+      d_i[d] = 1'b0;
+      t = $dist_uniform(seed, 30, 5000);
+      # t;
+      // folowed by longer pauses
+      d_i[d] = 1'b1;
+      t = $dist_uniform(seed, 10_000, 200_000);
+      # t;
+    end
+    // stable OFF state at the end for 50ms
+    d_i[d] = 1'b0;
+    # 50_000_000;
+    // end simulation
+    $finish();
+  end
+
+end endgenerate
+
+//// instantiate RTL
+//debouncer #(
+//  .CN   (CN)
+//) debouncer_i [DW-1:0] (
+//  .clk  (clk),
+//  .d_i  (d_i),
+//  .d_o  (d_o)
+//);
 
 endmodule
