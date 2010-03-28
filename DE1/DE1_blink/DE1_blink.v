efddf915c45805872d5414c2373c302acb5d6b81
Iztok Jeras <izi@marvin.(none)>
2010-03-28 20:13:06 +0200

    removed Terasic copiright, since there was no original code left, only adopted port definitions

--- DE1/DE1_blink/DE1_blink.v
+++ DE1/DE1_blink/DE1_blink.v
@@ -1,38 +1,6 @@
-// --------------------------------------------------------------------
-// Copyright (c) 2005 by Terasic Technologies Inc. 
-// --------------------------------------------------------------------
-//
-// Permission:
-//
-//   Terasic grants permission to use and modify this code for use
-//   in synthesis for all Terasic Development Boards and Altera Development 
-//   Kits made by Terasic.  Other use of this code, including the selling 
-//   ,duplication, or modification of any portion is strictly prohibited.
-//
-// Disclaimer:
-//
-//   This VHDL/Verilog or C/C++ source code is intended as a design reference
-//   which illustrates how these types of functions can be implemented.
-//   It is the user's responsibility to verify their design for
-//   consistency and functionality through the use of formal
-//   verification methods.  Terasic provides no warranty regarding the use 
-//   or functionality of this code.
-//
-// --------------------------------------------------------------------
-//           
-//                     Terasic Technologies Inc
-//                     356 Fu-Shin E. Rd Sec. 1. JhuBei City,
-//                     HsinChu County, Taiwan
-//                     302
-//
-//                     web: http://www.terasic.com/
-//                     email: support@terasic.com
-//
-// --------------------------------------------------------------------
-//
-// Major Functions:     DE1 NIOS
-//
-// --------------------------------------------------------------------
+//////////////////////////////////////////////////////////////////////////////
+// Copyright 2010 by Iztok Jeras (based on code by Terasic Technologies Inc.)
+//////////////////////////////////////////////////////////////////////////////
 
 module DE1_blink (
 // Clock Input
