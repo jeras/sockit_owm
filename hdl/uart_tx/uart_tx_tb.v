7feecc3940b42d3c7c312c8d4185e2c73139a910
Iztok Jeras <izi@marvin.(none)>
2010-04-04 12:00:15 +0200

    added UART monitor

--- hdl/uart_tx/uart_tx_tb.v
+++ hdl/uart_tx/uart_tx_tb.v
@@ -3,8 +3,8 @@
 module uart_tx_tb;
 
 // system clock parameters
-//localparam FRQ = 24_000_000;  // 24MHz // realistic option                                                                                                            
-localparam FRQ =  1_000_000;  //  1MHz // option for faster simulation
+//localparam real FRQ = 24_000_000;  // 24MHz // realistic option
+localparam real FRQ =  1_000_000;  //  1MHz // option for faster simulation
 localparam real CP = 1000000000/FRQ;  // clock period
 
 // Avalon MM parameters
@@ -13,7 +13,12 @@ localparam ADW = 32;     // data width
 localparam ABW = ADW/8;  // byte enable width
 
 // UART parameters
-localparam BAUDRATE = 9600;
+localparam      BYTESIZE = 8;
+localparam      PARITY   = "NONE";
+localparam      STOPSIZE = 1;
+// localparam real BAUDRATE = 9600;  // realistic baudrate value
+localparam real BAUDRATE = FRQ;    // baudrate for UART at system clock
+localparam real T_BIT = 1_000_000_000.0/BAUDRATE;  // T=1.0s/baudrate
 
 // system_signals
 reg            clk;  // clock
@@ -30,14 +35,15 @@ wire           avalon_waitrequest;  //
 wire           avalon_transfer;
 reg  [ADW-1:0] data;
 // UART
-//wire           uart_rx;
-wire           uart_tx;
+//wire           uart_RxD;
+wire           uart_TxD;
 
 // request for a dumpfile
 initial begin
   $dumpfile("uart_tx.vcd");
   $dumpvars(0, uart_tx_tb);
 end
+
 
 // clock generation
 initial        clk = 1'b1;
@@ -74,6 +80,7 @@ initial begin
   $finish(); 
 end
 
+
 // avalon transfer cycle generation task
 task avalon_cycle (
   input            r_w,  // 0-read or 1-write cycle
@@ -83,8 +90,7 @@ task avalon_cycle (
   output [ADW-1:0] rdt
 );
 begin
-  $write ("Avalon MM cycle: T_start=%10tns, %s transfer address=%08x byteenable=%04b writedata=%08x - ",
-                            $time/1000.0, r_w?"write":"read ", adr, ben,           wdt              );
+  $display ("Avalon MM cycle start: T=%10tns, %s address=%08x byteenable=%04b writedata=%08x", $time/1000.0, r_w?"write":"read ", adr, ben, wdt);
   // start an Avalon cycle
   avalon_read       <= ~r_w;
   avalon_write      <=  r_w;
@@ -98,13 +104,45 @@ begin
   avalon_write      <= 1'b0;
   // read data
   rdt = avalon_readdata;
-  $write ("readdata=%08x, T_stop=%10tns\n",
-           rdt,           $time/1000.0  );
+  $display ("Avalon MM cycle end  : T=%10tns, readdata=%08x", $time/1000.0, rdt);
 end
 endtask
 
 // avalon cycle transfer cycle end status
 assign avalon_transfer = (avalon_read | avalon_write) & ~avalon_waitrequest;
+
+
+// UART monitor (receiver) signals
+event     uart_sample;  // a semple event should be placed in the middle of each bit
+integer   uart_cnt;     // bit counter
+reg [7:0] uart_dat;     // byte of data
+
+// UART monitor (receiver)
+initial begin
+  wait (~rst);
+  while (1) begin
+    @ (negedge uart_TxD) begin
+      // wait half of bit time
+      #(T_BIT/2);
+      // check the start bit
+      if (uart_TxD != 1'b0)  $display ("UART: start bit error."); #T_BIT;
+      // sample in the middle of each bit
+      for (uart_cnt=0; uart_cnt<BYTESIZE; uart_cnt=uart_cnt+1) begin
+        -> uart_sample;  uart_dat [uart_cnt] = uart_TxD;  #T_BIT;
+      end
+      // check parity
+      case (PARITY)
+        "ODD"  : begin  if (uart_TxD != ~^uart_dat)  $display ("UART: parity error."); #T_BIT;  end
+        "EVEN" : begin  if (uart_TxD !=  ^uart_dat)  $display ("UART: parity error."); #T_BIT;  end
+        "NONE" : begin                                                                          end
+      endcase
+      // check the stop bit and display the transferred character
+      if (uart_TxD != 1'b1)  $display ("UART: stop bit error.");
+      else                   $display ("UART: transferred character \"%s\".", uart_dat);
+    end
+  end
+end
+
 
 // instantiate uart_tx RTL
 uart_tx #(
@@ -123,8 +161,8 @@ uart_tx #(
   .avalon_readdata     (avalon_readdata),
   .avalon_waitrequest  (avalon_waitrequest),
   // UART
-  //.uart_rx             (uart_rx),
-  .uart_tx             (uart_tx)
+  //.uart_rx             (uart_RxD),
+  .uart_tx             (uart_TxD)
 );
 
 endmodule
