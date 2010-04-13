673e2dabbe508669a978e61be16192b279fe4ac9
Iztok Jeras <izi@marvin.(none)>
2010-04-13 20:33:14 +0200

    uart compiles, some receiving works

--- hdl/uart/uart.v
+++ hdl/uart/uart.v
@@ -20,8 +20,8 @@ module uart #(
   output     [ADW-1:0] avalon_readdata,
   output               avalon_waitrequest,
   // receiver status
-  output               status_irq,  // interrupt
-  output               status_err,  // error
+  output reg           status_irq,  // interrupt
+  output reg           status_err,  // error
   // UART
   input                uart_rxd,  // receive
   output reg           uart_txd   // transmit
@@ -47,6 +47,9 @@ reg          [3:0] txd_cnt, rxd_cnt;  // transfer length counter
 reg [BYTESIZE-1:0] txd_dat, rxd_dat;  // data shift register
 reg                txd_prt, rxd_prt;  // parity register
 
+reg [BYTESIZE-1:0] data;
+reg                parity;
+
 //////////////////////////////////////////////////////////////////////////////
 // Avalon logic
 //////////////////////////////////////////////////////////////////////////////
@@ -55,8 +58,6 @@ reg                txd_prt, rxd_prt;  // parity register
 assign avalon_waitrequest = avalon_read | txd_run;
 assign avalon_trn_w = avalon_write & ~avalon_waitrequest;
 assign avalon_trn_r = avalon_read  & ~avalon_waitrequest;
-
-assign avalon_readdata = {ADW{1'b0}};
 
 //////////////////////////////////////////////////////////////////////////////
 // UART transmitter
@@ -90,17 +91,13 @@ end
 
 // data shift register
 always @ (posedge clk)
-begin
   if (avalon_trn_w)  txd_dat <= avalon_writedata[BYTESIZE-1:0];
   else if (txd_ena)  txd_dat <= {1'b1, txd_dat[BYTESIZE-1:1]};
-end
 
 // parity register
 always @ (posedge clk)
-begin
   if (avalon_trn_w)  txd_prt <= PRT;
   else if (txd_ena)  txd_prt <= txd_prt ^ txd_dat[0];
-end
 
 // output register
 always @ (posedge clk, posedge rst)
@@ -114,6 +111,74 @@ end
 // UART receiver
 //////////////////////////////////////////////////////////////////////////////
 
+reg uart_rxd_dly;
 
+// delay uart_rxd and detect a start negative edge
+always @ (posedge clk)
+uart_rxd_dly <= uart_rxd;
+
+assign rxd_start = uart_rxd_dly & ~uart_rxd;
+
+// baudrate generator from clock (it counts down to 0 generating a baud pulse)
+always @ (posedge clk, posedge rst)
+if (rst) rxd_bdr <= N_BIT-1;
+else begin
+  if (rxd_start)  rxd_bdr <= (N_BIT-1)>>1;
+  else            rxd_bdr <= ~|rxd_bdr ? N_BIT-1 : rxd_bdr - rxd_run;
+end
+
+// enable signal for shifting logic
+always @ (posedge clk, posedge rst)
+if (rst)  rxd_ena <= 1'b0;
+else      rxd_ena <= (rxd_bdr == 'd1);
+
+// bit counter
+always @ (posedge clk, posedge rst)
+if (rst)             rxd_cnt <= 0;
+else begin
+  if (avalon_trn_w)  rxd_cnt <= UTL;
+  else if (rxd_ena)  rxd_cnt <= rxd_cnt - 1;
+end
+
+// shift status
+always @ (posedge clk, posedge rst)
+if (rst)             rxd_run <= 1'b0;
+else begin
+  if (avalon_trn_w)  rxd_run <= 1'b1;
+  else if (rxd_ena)  rxd_run <= rxd_cnt != 4'd0;
+end
+
+// data shift register
+always @ (posedge clk)
+  if (rxd_ena)       rxd_dat <= {uart_rxd, rxd_dat[BYTESIZE-1:1]};
+
+// parity register
+always @ (posedge clk)
+  if (rxd_start)     rxd_prt <= PRT;
+  else if (rxd_ena)  rxd_prt <= rxd_prt ^ uart_rxd;
+
+assign rxd_end = ~|rxd_cnt & rxd_ena;
+
+// avalon read data and parity error
+always @ (posedge clk)
+  if (rxd_end) {parity, data} <= {rxd_prt, rxd_dat};
+
+// fifo interrupt status
+always @ (posedge clk, posedge rst)
+if (rst)                 status_irq <= 1'b0;
+else begin
+  if (rxd_end)           status_irq <= 1'b1;
+  else if (avalon_trn_r) status_irq <= 1'b0;
+end
+
+// fifo overflow error
+always @ (posedge clk, posedge rst)
+if (rst)                 status_err <= 1'b0;
+else begin
+  if (avalon_trn_r)      status_err <= 1'b0;
+  else if (rxd_end)      status_err <= status_irq;
+end
+
+assign avalon_readdata = {status_irq, status_err, {ADW-BYTESIZE-3{1'b0}}, parity, data};
 
 endmodule
