be5a5df2b08d3eec3e269210bd724787a750a441
Iztok Jeras <iztok.jeras@gmail.com>
2010-11-21 15:42:39 +0100

    added a basic validation callback

--- hdl/onewire/sockit_owm_hw.tcl
+++ hdl/onewire/sockit_owm_hw.tcl
@@ -19,6 +19,7 @@ set_module_property INSTANTIATE_IN_SYSTEM_MODULE true
 set_module_property EDITABLE true
 set_module_property ANALYZE_HDL TRUE
 
+set_module_property VALIDATION_CALLBACK  validation_callback
 set_module_property ELABORATION_CALLBACK elaboration_callback
 
 # TODO add_documentation_link
@@ -29,7 +30,7 @@ add_file sockit_owm.v {SYNTHESIS SIMULATION}
 # parameters
 add_parameter BDW INTEGER
 set_parameter_property BDW DESCRIPTION "CPU interface data bus width"
-set_parameter_property BDW DISPLAY_NAME BDW
+#set_parameter_property BDW DISPLAY_NAME BDW
 set_parameter_property BDW DISPLAY_HINT "radio"
 set_parameter_property BDW DEFAULT_VALUE 32
 set_parameter_property BDW ALLOWED_RANGES {8 32}
@@ -40,7 +41,7 @@ set_parameter_property BDW HDL_PARAMETER true
 
 add_parameter OVD_E BOOLEAN
 set_parameter_property OVD_E DESCRIPTION "Implementation of overdrive enable, disabling it can spare a small amount of logic."
-set_parameter_property OVD_E DISPLAY_NAME OVD_E
+#set_parameter_property OVD_E DISPLAY_NAME OVD_E
 set_parameter_property OVD_E DEFAULT_VALUE 1
 set_parameter_property OVD_E UNITS None
 set_parameter_property OVD_E AFFECTS_GENERATION false
@@ -48,18 +49,12 @@ set_parameter_property OVD_E HDL_PARAMETER true
 
 add_parameter OWN INTEGER
 set_parameter_property OWN DESCRIPTION "Nummber of 1-wire channels"
-set_parameter_property OWN DISPLAY_NAME OWN
+#set_parameter_property OWN DISPLAY_NAME OWN
 set_parameter_property OWN DEFAULT_VALUE 1
 set_parameter_property OWN ALLOWED_RANGES 1:16
 set_parameter_property OWN AFFECTS_GENERATION false
 set_parameter_property OWN AFFECTS_ELABORATION true
 set_parameter_property OWN HDL_PARAMETER true
-
-add_parameter F_CLK INTEGER
-set_parameter_property F_CLK SYSTEM_INFO {CLOCK_RATE clock_reset}
-set_parameter_property F_CLK DISPLAY_NAME F_CLK
-set_parameter_property F_CLK DESCRIPTION "System clock frequency"
-set_parameter_property F_CLK UNITS megahertz
 
 add_parameter BTP_N STRING
 set_parameter_property BTP_N DESCRIPTION "Base time period for normal mode"
@@ -73,13 +68,24 @@ set_parameter_property BTP_N HDL_PARAMETER true
 
 add_parameter BTP_O STRING
 set_parameter_property BTP_O DESCRIPTION "Base time period for overdrive mode"
-set_parameter_property BTP_O DISPLAY_NAME BTP_N
-#set_parameter_property BTP_O DISPLAY_HINT "radio"
+#set_parameter_property BTP_O DISPLAY_NAME BTP_N
+set_parameter_property BTP_O DISPLAY_HINT "radio"
 set_parameter_property BTP_O DEFAULT_VALUE "1.0"
 set_parameter_property BTP_O ALLOWED_RANGES {"1.0" "0.5"}
 set_parameter_property BTP_O UNITS microseconds
 set_parameter_property BTP_O AFFECTS_GENERATION false
 set_parameter_property BTP_O HDL_PARAMETER true
+
+add_parameter AUTO_CDR BOOLEAN
+set_parameter_property AUTO_CDR DISPLAY_NAME "Automatic dividers"
+set_parameter_property AUTO_CDR DESCRIPTION "Set to enable automatic divider computation from system clock frequency"
+set_parameter_property AUTO_CDR DEFAULT_VALUE 1
+
+add_parameter F_CLK INTEGER
+set_parameter_property F_CLK SYSTEM_INFO {CLOCK_RATE clock_reset}
+set_parameter_property F_CLK DISPLAY_NAME F_CLK
+set_parameter_property F_CLK DESCRIPTION "System clock frequency"
+set_parameter_property F_CLK UNITS megahertz
 
 add_parameter CDR_N INTEGER
 set_parameter_property CDR_N DESCRIPTION "Clock divider ratio for normal mode"
@@ -100,11 +106,10 @@ set_parameter_property CDR_O HDL_PARAMETER true
 
 add_display_item "Timing base options" BTP_N parameter
 add_display_item "Timing base options" BTP_O parameter
+add_display_item "Clock dividers" AUTO_CDR parameter
 add_display_item "Clock dividers" F_CLK parameter
 add_display_item "Clock dividers" CDR_N parameter
 add_display_item "Clock dividers" CDR_O parameter
-
-add
 
 # connection point clock_reset
 add_interface clock_reset clock end
@@ -160,8 +165,20 @@ add_interface_port ext onewire_p export Output 1
 add_interface_port ext onewire_e export Output 1
 add_interface_port ext onewire_i export Input  1
 
+proc validation_callback {} {
+  # disable editing od dividers if automatic computation is enabled
+  set auto [get_parameter_value AUTO_CDR]
+  if {$auto} {
+    set_parameter_property CDR_N ENABLED false
+    set_parameter_property CDR_O ENABLED false
+  } else {
+    set_parameter_property CDR_N ENABLED true
+    set_parameter_property CDR_O ENABLED true
+  }
+}
+
 proc elaboration_callback {} {
-  # Add software defines
+  # add software defines
   set_module_assignment embeddedsw.CMacro.OWN   [get_parameter_value OWN]
   set_module_assignment embeddedsw.CMacro.OVD_E [get_parameter_value OVD_E]
 }
