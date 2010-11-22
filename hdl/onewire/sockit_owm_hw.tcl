b942f4db09768cc882ff7df1a715c2dc32bc6c6e
Iztok Jeras <iztok.jeras@gmail.com>
2010-11-22 22:30:27 +0100

    onewire tcl script: added proper divider calculation

--- hdl/onewire/sockit_owm_hw.tcl
+++ hdl/onewire/sockit_owm_hw.tcl
@@ -19,7 +19,6 @@ set_module_property INSTANTIATE_IN_SYSTEM_MODULE true
 set_module_property EDITABLE true
 set_module_property ANALYZE_HDL TRUE
 
-set_module_property      EDITOR_CALLBACK      editor_callback
 set_module_property  VALIDATION_CALLBACK  validation_callback
 set_module_property ELABORATION_CALLBACK elaboration_callback
 
@@ -51,8 +50,9 @@ set_parameter_property OVD_E HDL_PARAMETER true
 add_parameter OWN INTEGER
 set_parameter_property OWN DESCRIPTION "Nummber of 1-wire channels"
 #set_parameter_property OWN DISPLAY_NAME OWN
+#set_parameter_property BTP_N DISPLAY_HINT "drop-down"
 set_parameter_property OWN DEFAULT_VALUE 1
-set_parameter_property OWN ALLOWED_RANGES 1:16
+set_parameter_property OWN ALLOWED_RANGES {1 2 3 4 5 6 7 8 9 10 11 12 13 14 15 16}
 set_parameter_property OWN AFFECTS_GENERATION false
 set_parameter_property OWN AFFECTS_ELABORATION true
 set_parameter_property OWN HDL_PARAMETER true
@@ -62,8 +62,7 @@ set_parameter_property BTP_N DESCRIPTION "Base time period for normal mode"
 #set_parameter_property BTP_N DISPLAY_NAME BTP_N
 set_parameter_property BTP_N DISPLAY_HINT "radio"
 set_parameter_property BTP_N DEFAULT_VALUE "5.0"
-set_parameter_property BTP_N ALLOWED_RANGES {"5.0" "7.5" "6.0"}
-set_parameter_property BTP_N UNITS microseconds
+set_parameter_property BTP_N ALLOWED_RANGES {"5.0:5.0us (prefered)" "7.5:7.5us" "6.0:6.0us - 7.5us"}
 set_parameter_property BTP_N AFFECTS_GENERATION false
 set_parameter_property BTP_N HDL_PARAMETER true
 
@@ -72,15 +71,9 @@ set_parameter_property BTP_O DESCRIPTION "Base time period for overdrive mode"
 #set_parameter_property BTP_O DISPLAY_NAME BTP_N
 set_parameter_property BTP_O DISPLAY_HINT "radio"
 set_parameter_property BTP_O DEFAULT_VALUE "1.0"
-set_parameter_property BTP_O ALLOWED_RANGES {"1.0" "0.5"}
-set_parameter_property BTP_O UNITS microseconds
+set_parameter_property BTP_O ALLOWED_RANGES {"1.0:1.0us (prefered)" "0.5:0.5us - 0.66us"}
 set_parameter_property BTP_O AFFECTS_GENERATION false
 set_parameter_property BTP_O HDL_PARAMETER true
-
-add_parameter AUTO_CDR BOOLEAN
-set_parameter_property AUTO_CDR DISPLAY_NAME "Automatic dividers"
-set_parameter_property AUTO_CDR DESCRIPTION "Set to enable automatic divider computation from system clock frequency"
-set_parameter_property AUTO_CDR DEFAULT_VALUE 1
 
 add_parameter F_CLK INTEGER
 set_parameter_property F_CLK SYSTEM_INFO {CLOCK_RATE clock_reset}
@@ -88,15 +81,15 @@ set_parameter_property F_CLK DISPLAY_NAME F_CLK
 set_parameter_property F_CLK DESCRIPTION "System clock frequency"
 set_parameter_property F_CLK UNITS megahertz
 
-add_parameter CDR_N INTEGER
+add_parameter CDR_N POSITIVE
+set_parameter_property CDR_N DERIVED true
 set_parameter_property CDR_N DESCRIPTION "Clock divider ratio for normal mode"
 set_parameter_property CDR_N DISPLAY_NAME CDR_N
 set_parameter_property CDR_N DEFAULT_VALUE 5
-set_parameter_property CDR_N UNITS None
 set_parameter_property CDR_N AFFECTS_GENERATION false
 set_parameter_property CDR_N HDL_PARAMETER true
 
-add_parameter CDR_O INTEGER
+add_parameter CDR_O POSITIVE
 set_parameter_property CDR_O DERIVED true
 set_parameter_property CDR_O DESCRIPTION "Clock divider ratio for overdrive mode"
 set_parameter_property CDR_O DISPLAY_NAME CDR_O
@@ -106,7 +99,6 @@ set_parameter_property CDR_O HDL_PARAMETER true
 
 add_display_item "Timing base options" BTP_N parameter
 add_display_item "Timing base options" BTP_O parameter
-add_display_item "Clock dividers" AUTO_CDR parameter
 add_display_item "Clock dividers" F_CLK parameter
 add_display_item "Clock dividers" CDR_N parameter
 add_display_item "Clock dividers" CDR_O parameter
@@ -166,44 +158,47 @@ add_interface_port ext onewire_e export Output 1
 add_interface_port ext onewire_i export Input  1
 
 proc validation_callback {} {
-  # check if automatic computation is enabled
-  set auto [get_parameter_value AUTO_CDR]
   # check if overdrive is enabled
   set ovd [get_parameter_value OVD_E]
-  #
-#  set_parameter_property CDR_N DERIVED [expr {$auto         ? "true" : "false"}]
-#  set_parameter_property CDR_O DERIVED [expr {$auto && $ovd ? "true" : "false"}]
-}
-
-proc editor_callback {} {
-  # check if automatic computation is enabled
-  set auto [get_parameter_value AUTO_CDR]
-  # check if overdrive is enabled
-  set ovd [get_parameter_value OVD_E]
-  send_message info "DEBUG: the OVD_E value is $ovd."
-  # get clock frequency
+  # get clock frequency in Hz
   set f [get_parameter_value F_CLK]
   # get base time periods
-  set t_n [get_parameter_value BTP_N]
-  set t_o [get_parameter_value BTP_O]
-  send_message info "DEBUG: the BTP_N value is $t_n, BTP_O value is $t_o."
+  set btp_n [get_parameter_value BTP_N]
+  set btp_o [get_parameter_value BTP_O]
   # disable editing od dividers
-  set_parameter_property CDR_N ENABLED [expr {$auto         ? "true" : "false"}]
-  set_parameter_property CDR_O ENABLED [expr {$auto && $ovd ? "true" : "false"}]
-  set_parameter_property BTP_O ENABLED [expr {         $ovd ? "true" : "false"}]
-  # perform velidation tasks
-  if {$auto} {
-    if {$ovd} {
-      # compute overdrive mode divider
-      if {$t_o=="1.0"} {
-        set d_o [expr {$f/1000000}]
-        set_parameter_value CDR_O $d_o
-        send_message info "DEBUG: the CDR_O value is $d_o."
-#        set_parameter_property CDR_O DEFAULT_VALUE $d_o
-      } elseif {$t_o=="0.5"} {
-      }
-    }
+  set_parameter_property BTP_O ENABLED [expr {$ovd ? "true" : "false"}]
+  # compute normal mode divider
+  if {$btp_n=="5.0"} {
+    set d_n [expr {$f/200000}]
+    set t_n [expr {1000000.0/($f/$d_n)}]
+    set e_n [expr {($t_n-5.0)/5.0}]
+  } elseif {$btp_n=="7.5"} {
+    set d_n [expr {$f/133333}]
+    set t_n [expr {1000000.0/($f/$d_n)}]
+    set e_n [expr {($t_n-7.5)/7.5}]
+  } elseif {$btp_n=="6.0"} {
+    set d_n [expr {$f/133333}]
+    set t_n [expr {1000000.0/($f/$d_n)}]
+    set e_n [expr {($t_n-7.5)/7.5}]
   }
+  # compute overdrive mode divider
+  if {$btp_o=="1.0"} {
+    set d_o [expr {$f/1000000}]
+    set t_o [expr {1000000.0/($f/$d_o)}]
+    set e_o [expr {($t_o-1.0)/1.0}]
+  } elseif {$btp_o=="0.5"} {
+    set d_o [expr {$f/2000000}]
+    set t_o [expr {1000000.0/($f/$d_o)}]
+    set e_o [expr {($t_o-0.5)/0.5}]
+  }
+  # set divider values
+             set_parameter_value CDR_N $d_n
+  if {$ovd} {set_parameter_value CDR_O $d_o}
+  # report timing error
+  send_message info "DEBUG: the normal    mode 'base time period' is $t_n."
+  send_message info "DEBUG: the overdrive mode 'base time period' is $t_o."
+  send_message info "DEBUG: the normal    mode error is $e_n."
+  send_message info "DEBUG: the overdrive mode error is $e_o."
 }
 
 proc elaboration_callback {} {
